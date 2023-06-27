556fcc18d3712ad840bf10e4497962ce78eb7a2f
Hyeongseob Jeong <jhseob96@gmail.com>
2023-06-27 17:49:25 +0900

    switch domain

--- src/const/klaytn.ts
+++ src/const/klaytn.ts
@@ -1,7 +1,7 @@
 import Web3 from 'web3';
 
-const KLAYTN_CYPRESS_RPC = 'https://klaytn-mainnet-rpc.allthatnode.com:8551/8556797ngIBvFv0VQ5uJIFQoc8jPKpSM';
-const KLAYTN_BAOBAB_RPC = 'https://klaytn-baobab-rpc.allthatnode.com:8551/8556797ngIBvFv0VQ5uJIFQoc8jPKpSM';
+const KLAYTN_CYPRESS_RPC = 'https://klaytn-mainnet-rpc.allthatnode.com:8551/Z4uSyu9UgJKl3jSP1F2J5KZeeroaSK1T';
+const KLAYTN_BAOBAB_RPC = 'https://klaytn-mainnet-rpc.allthatnode.com:8551/Z4uSyu9UgJKl3jSP1F2J5KZeeroaSK1T';
 
 const provider = new Web3.providers.HttpProvider(KLAYTN_BAOBAB_RPC);
 export const web3 = new Web3(window.ethereum || provider);
