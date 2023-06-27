8c38b99d3f2e459767f24365336ca1b566026412
Hyeongseob Jeong <jhseob96@gmail.com>
2023-06-27 18:01:32 +0900

    swtich domain

--- src/const/klaytn.ts
+++ src/const/klaytn.ts
@@ -4,4 +4,4 @@ const KLAYTN_CYPRESS_RPC = 'https://klaytn-mainnet-rpc.allthatnode.com:8551/Z4uS
 const KLAYTN_BAOBAB_RPC = 'https://klaytn-mainnet-rpc.allthatnode.com:8551/Z4uSyu9UgJKl3jSP1F2J5KZeeroaSK1T';
 
 const provider = new Web3.providers.HttpProvider(KLAYTN_BAOBAB_RPC);
-export const web3 = new Web3(window.ethereum || provider);
+export const web3 = new Web3(provider);
